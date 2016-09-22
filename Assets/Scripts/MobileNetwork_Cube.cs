83e789195b7a43f2f51338e920b549ef306737da
Windy Darian <p123456638@msn.com>
2016-09-22 01:58:18 -0400

    enables player to choose room

--- Assets/Scripts/MobileNetwork_Cube.cs
+++ Assets/Scripts/MobileNetwork_Cube.cs
@@ -3,8 +3,10 @@
 public class MobileNetwork_Cube : Photon.PunBehaviour
 {
     // TODO-1.b: write any functions needed to establish connection
-    //   and join a room 
-
+    //   and join a room. Joining a random room will do for now if you are testing
+    //   it yourself. But you can also list the rooms or require player to enter
+    //   the room name in case there are more people playing
+    //   your game - though it is not required for the assignment.
 
     void OnGUI()
     {
--- Assets/Scripts/PCNetwork_Cube.cs
+++ Assets/Scripts/PCNetwork_Cube.cs
@@ -4,6 +4,8 @@ using System.Collections;
 // LOOK-1.b: the parent class is not a basic MonoBehavior!
 public class PCNetwork_Cube : Photon.PunBehaviour
 {
+    string roomName;
+
     // LOOK-1.b: creating a room on PC
     void Start()
     {
@@ -12,16 +14,34 @@ public class PCNetwork_Cube : Photon.PunBehaviour
         //   so the application will automatically connect to Lobby
         //   and call OnJoinedLobby()
         PhotonNetwork.ConnectUsingSettings("0.1");
+        roomName = GenerateRoomName();
+    }
+
+    static string GenerateRoomName()
+    {
+        const string characters = "abcdefghijklmnopqrstuvwxyz0123456789"; //add the characters you want
+
+        string result = "";
+
+        int charAmount = Random.Range(4, 6); //set those to the minimum and maximum length of your string
+        for (int i = 0; i < charAmount; i++)
+        {
+            result += characters[Random.Range(0, characters.Length)];
+        }
+
+        return result;
     }
 
     void OnGUI()
     {
         GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+        GUILayout.Label("Room Name: " + roomName);
     }
 
     public override void OnJoinedLobby()
     {
-        PhotonNetwork.CreateRoom(null);
+        //PhotonNetwork.CreateRoom(null);
+        PhotonNetwork.CreateRoom(roomName);
     }
 
     // Look-1.b: We are not doing anything in the functions below
